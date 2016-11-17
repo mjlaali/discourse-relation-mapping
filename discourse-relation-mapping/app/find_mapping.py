8d9684c3c5228121471b6b0b15ddb3492930813f
Majid Laali <mjlaali@gmail.com>
2016-11-17 13:19:02 -0500

    add comments

--- discourse-relation-mapping/app/find_mapping.py
+++ discourse-relation-mapping/app/find_mapping.py
@@ -5,7 +5,7 @@ import numpy as np
 class Lexconn:
 
     def __init__(self, emission, entries):
-        """ initialize lexconn entries
+        """ initialize LEXCONN entries
         Arg:
             emission: the emission matrix of discourse connectives for PDTB relations,
                 in each cell emission(i, j) = P(PDTB_j|DC_i)
@@ -22,10 +22,10 @@ class Lexconn:
         pass
 
     @staticmethod
-    def __one_hot_encoding(ar, range):
+    def __one_hot_encoding(ar, ar_range):
         values = ar
         values.shape = (ar.shape[0], 1)
-        one_hot = ar == np.arange(range)
+        one_hot = ar == np.arange(ar_range)
         return one_hot.astype(np.float32)
 
     def get_mapping(self):
@@ -33,15 +33,18 @@ class Lexconn:
         Return:
             a mapping between PDTB relations and RST relations. Mapping[i, j] = P(RST_j|PDTB_i)
         """
+        
         sdc = Lexconn.__one_hot_encoding(self.__entries[:, 0], self.__dc_cnt)
         src = Lexconn.__one_hot_encoding(self.__entries[:, 1], self.__rst_cnt)
 
-        sdc_e = np.dot(sdc, self.__emission)
-        m_logits = tf.Variable(np.random.rand(self.__pdtb_cnt, self.__rst_cnt).astype(np.float32))
-        m = tf.nn.softmax(m_logits)
-        score = tf.matmul(sdc_e, tf.matmul(m, src.T))
+        sdc_e = np.dot(sdc, self.__emission) #select emission row for each entry in LEXCONN
+        init_value = np.random.rand(self.__pdtb_cnt, self.__rst_cnt).astype(np.float32)
+        m_logits = tf.Variable(init_value)  #parameters of mapping probabilities
+        m = tf.nn.softmax(m_logits)     #Mapping probabilities
+        src_m = tf.matmul(m, src.T)     #select mapping rows for each entry in LEXCONN
+        score = tf.matmul(sdc_e, src_m) #calculate the probabilities of all possible combination of connectives and relations
         log_score = tf.log(score)
-        sum_log_score = tf.trace(log_score)
+        sum_log_score = tf.trace(log_score) #we only need those that have the same index in LEXCONN
         optimizer = tf.train.AdamOptimizer(0.01).minimize(tf.neg(sum_log_score))
         init = tf.initialize_all_variables()
         with tf.Session() as sess:
--- discourse-relation-mapping/test/test_find_mapping.py
+++ discourse-relation-mapping/test/test_find_mapping.py
@@ -14,9 +14,11 @@ class Test_Lexconn (unittest.TestCase):
 
     def test_mapping_when_one_dc_one_entry(self):
         emission = np.array([[0.8, 0.2], [0.4, 0.6]], dtype=np.float32)
+        print(emission)
         entries = np.array([[0, 0], [0, 1], [1, 1], [1, 2]])
         lexconn = Lexconn(emission, entries)
         got_mapping = lexconn.get_mapping()
+        print(got_mapping)
         expected_mapping = np.array([[0.4267, 0.5733, 0.0], [0.0, 0.3963, 0.6037]])
         npt.assert_almost_equal(got_mapping, expected_mapping, decimal=4)
         pass
